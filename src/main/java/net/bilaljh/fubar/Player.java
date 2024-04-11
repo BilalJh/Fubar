2b41bc62485574f1eaa0e3e23e65216bcf3ecc05
Bilal Jh <bilal.j@live.de>
2024-04-11 22:07:29 +0200

    Added Player.java

---
+++ src/main/java/net/bilaljh/fubar/Player.java
@@ -0,0 +1,13 @@
+package net.bilaljh.fubar;
+
+public class Player {
+
+    int posX;
+    int posY;
+
+    public Player(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+}

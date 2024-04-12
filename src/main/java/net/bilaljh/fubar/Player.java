2aea15eaad16cc96a01438785740e0fab5c1f29e
Bilal Jh <bilal.j@live.de>
2024-04-12 22:38:58 +0200

    Added get and set methods

--- src/main/java/net/bilaljh/fubar/Player.java
+++ src/main/java/net/bilaljh/fubar/Player.java
@@ -2,12 +2,24 @@ package net.bilaljh.fubar;
 
 public class Player {
 
-    int posX;
-    int posY;
+    private double posX;
+    private double posY;
 
-    public Player(int posX, int posY) {
+    public Player(double posX, double posY) {
         this.posX = posX;
         this.posY = posY;
     }
 
+    public double getPosX() {
+        return posX;
+    }
+    public void setPosX(double posX) {
+        this.posX = posX;
+    }
+    public double getPosY() {
+        return posY;
+    }
+    public void setPosY(double posY) {
+        this.posY = posY;
+    }
 }

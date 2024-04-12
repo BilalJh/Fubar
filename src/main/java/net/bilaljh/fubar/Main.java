d7f6f6b3ab2ea267bcf0ece370c16c3375b7c7ba
Bilal Jh <bilal.j@live.de>
2024-04-12 22:38:28 +0200

    Minor Changes and relocated displaying to Display.java

--- src/main/java/net/bilaljh/fubar/Main.java
+++ src/main/java/net/bilaljh/fubar/Main.java
@@ -1,16 +1,13 @@
 package net.bilaljh.fubar;
 
 import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.scene.shape.Line;
-import javafx.stage.StageStyle;
-
 
 public class Main extends Application {
+
+    public static Raycaster engine;
+    public static Player player;
+    public static Display display;
 
     public static void main(String[] args) {
         Application.launch(args);
@@ -18,50 +15,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Stage primaryStage = new Stage();
-        Group root = new Group();
-        Scene primaryScene = new Scene(root, Color.BLACK);
-
-        Image icon = new Image("LS00.png");
-
-        primaryStage.getIcons().add(icon);
-        primaryStage.setTitle("Fubar! - PreAlpha v0.1");
-        primaryStage.setWidth(960);
-        primaryStage.setHeight(640);
-        primaryStage.setResizable(false);
-        //primaryStage.initStyle(StageStyle.UNDECORATED);
-
-        Line[] lines = new Line[961];
-        for (int i = 0; i <= 960; i++) {
-            lines[i] = new Line();
-            drawLine(lines[i], i, 640 / 2 - 100, i, 640 / 2 + 100, 1, Color.WHITE, root);
-        }
-
-        /*  -- How to draw a Line --
-            Line line = new Line();
-            line.setStartX();
-            line.setStartY();
-            line.setEndX();
-            line.setEndY();
-            line.setStrokeWidth();
-            line.setStroke();
-
-            root.getChildren().add(line);
-
-         */
+        player = new Player(300, 100);
+        engine = new Raycaster();
+        display = new Display();
 
 
-        primaryStage.setScene(primaryScene);
-        primaryStage.show();
-    }
-
-    public void drawLine(Line line, int startX, int startY, int endX, int endY, int width, Color color, Group root) {
-        line.setStartX(startX);
-        line.setStartY(startY);
-        line.setEndX(endX);
-        line.setEndY(endY);
-        line.setStrokeWidth(width);
-        line.setStroke(color);
-        root.getChildren().add(line);
     }
 }

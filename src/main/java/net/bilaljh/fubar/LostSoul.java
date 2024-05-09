e09087ff7c936d822e2afb8ccb5d47d29d01290d
bilaljh <bilal.j@live.de>
2024-05-09 23:47:29 +0200

    Major Changes

--- src/main/java/net/bilaljh/fubar/LostSoul.java
+++ src/main/java/net/bilaljh/fubar/LostSoul.java
@@ -1,17 +1,32 @@
 package net.bilaljh.fubar;
 
-public class LostSoul {
+public class LostSoul extends Actor{
 
-    String spriteFile;
-    int posX;
-    int posY;
+    public final int HEIGHT = Main.SCREEN_HEIGHT;
 
-    public LostSoul(int posX, int posY) {
-        spriteFile = "src/assets/LostSoul/LS00.png";
+    public LostSoul(int posX, int posY, double angle) {
         this.posX = posX;
         this.posY = posY;
+        this.angle = angle;
+
     }
 
+    public void idle(double screenX, double screenY) {
+        Player player = Main.player;
 
+        double distance = Math.sqrt(((posX - player.getPosX()) * (posX - player.getPosX())) + ((posY - player.getPosY()) * (posY - player.getPosY())));
+        double height = (16 * HEIGHT) / distance;
 
+        if(height > HEIGHT) {
+            height = HEIGHT;
+        }
+
+        Main.display.drawEnemy("file:src/resource/LS00.png", screenX, screenY, distance, Main.display.root);
+    }
+
+    public void die() {
+        Player player = Main.player;
+        player.setScore(player.getScore() + 100);
+        System.out.println(player.getScore());
+    }
 }

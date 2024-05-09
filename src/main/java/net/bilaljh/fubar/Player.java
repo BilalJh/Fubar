9c541a921d60b27f4382b245aacd965de4f77a68
bilaljh <bilal.j@live.de>
2024-05-09 23:48:27 +0200

    Added firing mechanic

--- src/main/java/net/bilaljh/fubar/Player.java
+++ src/main/java/net/bilaljh/fubar/Player.java
@@ -2,9 +2,12 @@ package net.bilaljh.fubar;
 
 public class Player extends Actor {
 
-    private int[][] map = Main.map.map;
+    private int[][] map;
 
     public Player(double posX, double posY, double angle, int life) {
+        map = Main.map.map;
+        score = 0;
+
         this.posX = posX;
         this.posY = posY;
         cellX = (int) (posX / 64);
@@ -13,29 +16,8 @@ public class Player extends Actor {
         this.life = life;
     }
 
-//    public void moveForward() {
-//        int collisionX = (int) (posX + 15);
-//        int collisionY = (int) (posY + 15);
-//        if(map[collisionX / 64][collisionY / 64] == 0){
-//            deltaX = Math.cos(angle);
-//            deltaY = Math.sin(angle);
-//            posX += deltaX * 10;
-//            posY += deltaY * 10;
-//            cellX = (int) (posX / 64);
-//            cellY = (int) (posY / 64);
-//            life--;
-//        }
-//    }
-//    public void moveBackward() {
-//        int collisionX = (int) (posX - 15);
-//        int collisionY = (int) (posY - 15);
-//        if(map[collisionX / 64][collisionY / 64] == 0){
-//            deltaX = Math.cos(angle);
-//            deltaY = Math.sin(angle);
-//            posX -= deltaX * 5;
-//            posY -= deltaY * 5;
-//            cellX = (int) (posX / 64);
-//            cellY = (int) (posY / 64);
-//        }
-//    }
+    public void fire() {
+        Main.engine.castShot();
+        System.out.println("Syso");
+    }
 }

b5e2f33fcb3e83c344f657c1cc04c30de090b8b4
bilaljh <bilal.j@live.de>
2024-05-13 00:52:23 +0200

    Minor changes

--- src/main/java/net/bilaljh/fubar/LostSoul.java
+++ src/main/java/net/bilaljh/fubar/LostSoul.java
@@ -1,32 +1,58 @@
 package net.bilaljh.fubar;
 
-public class LostSoul extends Actor{
+import javafx.scene.paint.Color;
 
-    public final int HEIGHT = Main.SCREEN_HEIGHT;
+public class LostSoul extends Actor{
 
     public LostSoul(int posX, int posY, double angle) {
         this.posX = posX;
         this.posY = posY;
         this.angle = angle;
-
     }
 
-    public void idle(double screenX, double screenY) {
+    public void show(double screenX, double screenY) {
+        Player player = Main.player;
+        double distance = Math.sqrt(((posX - player.getPosX()) * (posX - player.getPosX())) + ((posY - player.getPosY()) * (posY - player.getPosY())));
+
+        if(!(player.getPosX() == posX && player.getPosY() == posY)) {
+            Main.display.drawEnemy("file:src/resource/LS00.png", screenX, screenY, distance, Main.display.root);
+        }
+    }
+
+    public void idle() {
         Player player = Main.player;
 
-        double distance = Math.sqrt(((posX - player.getPosX()) * (posX - player.getPosX())) + ((posY - player.getPosY()) * (posY - player.getPosY())));
-        double height = (16 * HEIGHT) / distance;
+        angle = Math.atan2(player.getPosY() - posY, player.getPosX() - posX);
 
-        if(height > HEIGHT) {
-            height = HEIGHT;
+        if(player.getLife() <= 0) {
+            player.setLife(0);
+            Main.gameOver = true;
         }
 
-        Main.display.drawEnemy("file:src/resource/LS00.png", screenX, screenY, distance, Main.display.root);
+        move(1, true);
+        if(((int) (player.getPosX()) == (int) (posX)) && ((int) (player.getPosY()) == (int) (posY))) {
+            attack();
+        }
+    }
+
+    public void attack() {
+        Player player = Main.player;
+        Display display = Main.display;
+
+        player.setLife(player.getLife() - 25);
+        if(player.getLife() <= 0) {
+            player.setLife(0);
+            Main.gameOver = true;
+        }
+        display.drawRect(0, 0, Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, Color.RED, 0.5, Main.display.root);
+        respawn();
     }
 
     public void die() {
         Player player = Main.player;
         player.setScore(player.getScore() + 100);
         System.out.println(player.getScore());
+
+        respawn();
     }
 }

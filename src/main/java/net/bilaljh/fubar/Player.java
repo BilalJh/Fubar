5f30ec9c49096d94aae4fdb25b6e52dc56659c7c
bilaljh <bilal.j@live.de>
2024-05-13 00:53:05 +0200

    Removed Cells

--- src/main/java/net/bilaljh/fubar/Player.java
+++ src/main/java/net/bilaljh/fubar/Player.java
@@ -4,16 +4,14 @@ public class Player extends Actor {
 
     private int[][] map;
 
-    public Player(double posX, double posY, double angle, int life) {
+    public Player(double posX, double posY, double angle) {
         map = Main.map.map;
         score = 0;
 
         this.posX = posX;
         this.posY = posY;
-        cellX = (int) (posX / 64);
-        cellY = (int) (posY / 64);
         this.angle = angle; //In Bogenmaß!
-        this.life = life;
+        setLife(100);
     }
 
     public void fire() {

dd144bd63fc3a1ca4bf2b7156c0e531979e244c7
Bilal Jh <>
2025-04-18 12:15:59 +0200

    removed IDE files

--- out/artifacts/Fubar_jar/Fubar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Setze den Pfad zu deinem JAR und den Lib-Ordner
-
-set LIB_PATH=lib\*
-
-:: Starte das Java-Programm mit den richtigen Optionen
-java --module-path lib/lib --add-modules javafx.controls,javafx.fxml,javafx.media,javafx.web -cp "%JAR_PATH%;%LIB_PATH%" -jar Fubar.jar
-
-pause
--- out/artifacts/Fubar_jar/Fubar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java --module-path "lib/lib" --add-modules javafx.controls,javafx.fxml,javafx.media,javafx.web -jar "Fubar.jar"
